cc73abde2cb012dddb49796208ec7a34331c9bd5
Blickwinkel <1196061706@qq.com>
2018-08-17 17:18:59 +0800

    yx: ver 0.5

--- TraceSrc/views.py
+++ TraceSrc/views.py
@@ -6,8 +6,10 @@ import json
 # index front-to-end connector
 def index(req):
     ctx = dict()
-    startNum = 0
+    startNum = 34
+    # 在这里调整追踪0还是溯源1
+    # 前端只需要在这里处理即可
     direct = {'forwards':0, 'backwards':1}
-    tracePath = findPath.process('V%d' % startNum, direct['forwards'])
+    tracePath = findPath.process('V%d' % startNum, direct['backwards'])
     ctx['graph'] = py2js.parse(tracePath)
     return render(req, r'../templates/pages/graph.html', ctx)
