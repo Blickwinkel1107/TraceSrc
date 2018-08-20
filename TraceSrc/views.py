8193550fab4854d9876bb45861803f6e73a04dfc
unknown <1151545191@qq.com>
2018-08-20 17:39:10 +0800

    1.显示当前节点及溯源方向 2.右击菜单 3.点击菜单选择对当前节点操作

--- TraceSrc/views.py
+++ TraceSrc/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from TraceSrc.Algorithm import findPath
 from TraceSrc.Algorithm import py2js
 from django.views.decorators.csrf import csrf_exempt
-
+ctx = dict()
 # index front-to-end connector
 @csrf_exempt
 def index(req):
-    ctx = dict()
     # 在这里调整追踪0还是溯源1
     # 前端只需要在这里处理即可
     direct = {'forwards':0, 'backwards':1}
@@ -24,9 +23,17 @@ def index(req):
         pyPath = findPath.process('V%d' % startNum, direct[directNum])
         jsPath = py2js.parse(pyPath)
         f.write(jsPath)
-        f.close()
+        f.close()  
+        if int(req.POST['point']) == 0:
+            ctx['current_start'] = "1"
+            ctx['current_direct'] = '显示全图'
+        else:
+            ctx['current_start'] = int(req.POST['point'])
+            if req.POST['direct'] == "forwards":
+                ctx['current_direct'] = '向下追踪'
+            else:
+                ctx['current_direct'] = '向上溯源'
     f = open(r'templates/scripts/args.txt', 'r')
     ctx['graph'] = f.read()
-    # print(ctx['graph'])
     f.close()
     return render(req, r'../templates/pages/graph.html', ctx)
